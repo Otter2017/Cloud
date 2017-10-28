e167823b8f20ed28c385eb6e45bcd8d5175dffa7
OTTER <893812347@qq.com>
2017-10-28 08:57:27 +0800

    从EurekaServer发现ConfigServer的服务，并获取配置文件。 不需要再在bootstrap中配置ConfigServer的地址

--- configclient/src/main/java/com/ai/cloud/configclient/App.java
+++ configclient/src/main/java/com/ai/cloud/configclient/App.java
@@ -1,10 +1,34 @@
 package com.ai.cloud.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
+@EnableDiscoveryClient
 @SpringBootApplication
-public class App {
+@RestController
+public class App {	
+	@Value("${spring.application.name}")
+	String applicationName;
+	
+	@Value("${application.version}")
+	String applicationVersion;
+	
+	@Value("${profile}")
+	String profile;
+	@RequestMapping("/")
+	String home(){
+		return "Application Name:"+applicationName
+				+System.lineSeparator()
+				+"Application Version:"+applicationVersion
+				+System.lineSeparator()
+				+"Profile:"+profile;
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
