8cad80ec2e7f414b06d288c4f1067199380b3518
Otter2017 <893812347@qq.com>
2018-04-11 09:14:03 +0800

    delete eclipse setting files Change main class names

---
+++ configserver/src/main/java/com/ai/cloud/configserver/ConfigServerApp.java
@@ -0,0 +1,35 @@
+package com.ai.cloud.configserver;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableConfigServer
+@RestController
+@EnableEurekaClient
+public class ConfigServerApp {
+	@Value("${spring.application.name}")
+	String applicationName="Config Server";
+	
+	@RequestMapping("/")
+	String home(){
+		return "Welcome to "+applicationName;
+	}
+	
+	@RequestMapping("/time")
+	String time(){
+		return new Date().toString();
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApp.class, args);
+	}
+
+}
